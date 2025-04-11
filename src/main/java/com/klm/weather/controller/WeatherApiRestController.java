386dfb50a5a67f1f2682e44c547eb19439edb0e0
shagunsareen <shagunsareen3@gmail.com>
2025-04-11 15:53:25 +0530

    Added create weather & get weather by id apis

--- src/main/java/com/klm/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/klm/weather/controller/WeatherApiRestController.java
@@ -1,15 +1,35 @@
 package com.klm.weather.controller;
 
 import com.klm.weather.dto.WeatherDto;
+import com.klm.weather.model.Weather;
 import com.klm.weather.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherApiRestController {
     private final WeatherService weatherService;
 
+    @Autowired
     public WeatherApiRestController(WeatherService weatherService) {
         this.weatherService = weatherService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Weather> createWeather(@RequestBody WeatherDto weatherDto) {
+        Weather response = weatherService.createWeather(weatherDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Weather> getWeatherById(@PathVariable Integer id) {
+        Weather weather = weatherService.getWeatherById(id);
+        return ResponseEntity.ok(weather);
     }
 }
--- src/main/java/com/klm/weather/service/WeatherService.java
+++ src/main/java/com/klm/weather/service/WeatherService.java
@@ -1,13 +1,37 @@
 package com.klm.weather.service;
 
+import com.klm.weather.dto.WeatherDto;
+import com.klm.weather.model.Weather;
 import com.klm.weather.repository.WeatherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.klm.weather.exception.ResourceNotFoundException;
 
 @Service
 public class WeatherService {
     private final WeatherRepository weatherRepository;
 
+    @Autowired
     public WeatherService(WeatherRepository weatherRepository) {
         this.weatherRepository = weatherRepository;
+    }
+
+    public Weather createWeather(WeatherDto weatherDto) {
+        Weather weather = convertDtoToWeather(weatherDto);
+        return weatherRepository.save(weather);
+    }
+
+    private Weather convertDtoToWeather(WeatherDto weatherDto) {
+        return new Weather(weatherDto.getDate(),
+                weatherDto.getLat(),
+                weatherDto.getLon(),
+                weatherDto.getCity(),
+                weatherDto.getState(),
+                weatherDto.getTemperatures());
+    }
+
+    public Weather getWeatherById(Integer id) {
+        return weatherRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Weather with ID " + id + " not found"));
     }
 }
